000d5704107d96cc54483065a224efdb0bada770
itpex-ops <it@parcelex.in>
2026-05-06 19:20:10 +0530

    save changes

--- crmftl/menu.py
+++ crmftl/menu.py
@@ -1,6 +1,6 @@
 ROLE_MENUS = {
 
-    'superadmin': ['dashboard', 'enquiry', 'orders', 'vehicles', 'accounts','vehicle_accounts','accounts_customer','accounts_bank'],
+    'superadmin': ['dashboard', 'enquiry', 'orders', 'vehicles', 'accounts','vehicle_accounts','accounts_customer','accounts_bank','orders_manual'],
 
     'admin': ['dashboard', 'enquiry', 'orders', 'vehicles','orders_manual'],
 
