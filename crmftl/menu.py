08e2b2651dceef0578aff9cf4badf9ff2d1999ec
itpex-ops <it@parcelex.in>
2026-05-21 18:51:59 +0530

    updated base

--- crmftl/menu.py
+++ crmftl/menu.py
@@ -1,14 +1,14 @@
 ROLE_MENUS = {
 
-    'superadmin': ['dashboard','enquiry','enquiry_list', 'orders', 'vehicles', 'accounts','accounts_vehicle','accounts_customer','accounts_bank','orders_manual'],
+    'superadmin': ['dashboard','enquiry_create','enquiry_list', 'orders', 'vehicles', 'accounts','accounts_vehicle','accounts_customer','accounts_bank','orders_manual'],
 
-    'admin': ['dashboard','enquiry', 'orders_list', 'vehicles'],
+    'admin': ['dashboard','enquiry_create', 'orders_list', 'vehicles'],
 
     'fleet': ['dashboard', 'vehicles', 'orders_list','accounts_vehicle'],
 
-    'support': ['dashboard','enquiry','enquiry_list', 'orders', 'vehicles', 'accounts','accounts_vehicle','accounts_customer','accounts_bank','orders_manual'],
+    'support': ['dashboard','enquiry_create','enquiry_list', 'orders', 'vehicles', 'accounts','accounts_vehicle','accounts_customer','accounts_bank','orders_manual'],
 
-    'sales': ['dashboard','enquiry', 'orders', 'vehicle_tracking'],
+    'sales': ['dashboard','enquiry_create', 'orders', 'vehicle_tracking'],
 
     'accounts': ['dashboard', 'orders','accounts_vehicle','accounts_customer','accounts_bank','orders_manual'],
 
