f7583b301b06e21a7e08cceeaa21e441f72b394c
itpex-ops <it@parcelex.in>
2026-05-21 19:01:04 +0530

    updated base

--- crmftl/menu.py
+++ crmftl/menu.py
@@ -4,7 +4,7 @@ ROLE_MENUS = {
 
     'admin': ['dashboard','enquiry_create', 'orders_list', 'vehicles'],
 
-    'fleet': ['dashboard', 'vehicles', 'orders_list','accounts_vehicle'],
+    'fleet': ['dashboard', 'vehicles_placement','vehicle_tracking', 'orders_list','accounts_vehicle'],
 
     'support': ['dashboard','enquiry_create','enquiry_list', 'orders', 'vehicles', 'accounts','accounts_vehicle','accounts_customer','accounts_bank','orders_manual'],
 
