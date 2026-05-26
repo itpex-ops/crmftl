8290f510dbb9455b2c7e884694c96312008b15f0
itpex-ops <it@parcelex.in>
2026-05-26 16:29:05 +0530

    updated rights-admin

--- crmftl/menu.py
+++ crmftl/menu.py
@@ -2,7 +2,7 @@ ROLE_MENUS = {
 
     'superadmin': ['dashboard','enquiry_create','enquiry_list','orders_list','orders_manual','vehicles_placement','vehicle_tracking','accounts_vehicle','accounts_customer','accounts_bank'],
 
-    'admin': ['dashboard','enquiry_list', 'orders_list','vehicles_placement','vehicle_tracking'],
+    'admin': ['dashboard','enquiry_create','enquiry_list', 'orders_list','orders_manual','vehicles_placement','vehicle_tracking','accounts_vehicle','accounts_customer','accounts_bank'],
 
     'fleet': ['dashboard', 'vehicles_placement','vehicle_tracking', 'orders_list','accounts_vehicle'],
 
