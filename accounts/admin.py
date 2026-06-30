9864f2c235c666b22ba0ff455d759138aec96a35
itpex-ops <it@parcelex.in>
2026-06-30 15:06:56 +0530

    accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,6 +2,45 @@ from django.contrib import admin
 from .models import CustomerTransaction, VehicleTransaction
 
 
+@admin.register(CustomerTransaction)
+class CustomerTransactionAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "id",
+        "enquiry",
+        "customer_name",
+        "amount",
+        "payment_against",
+        "account_type",
+        "payment_mode",
+        "reference_no",
+        "transaction_datetime",
+        "created_by",
+    )
+
+    list_filter = (
+        "payment_against",
+        "account_type",
+        "payment_mode",
+        "transaction_datetime",
+    )
+
+    search_fields = (
+        "enquiry__enquiry_no",
+        "enquiry__customer_name",
+        "reference_no",
+        "remarks",
+    )
+
+    ordering = ("-id",)
+
+    date_hierarchy = "transaction_datetime"
+
+    @admin.display(description="Customer")
+    def customer_name(self, obj):
+        return obj.enquiry.customer_name
+
+
 @admin.register(VehicleTransaction)
 class VehicleTransactionAdmin(admin.ModelAdmin):
 
