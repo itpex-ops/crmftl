5acdcc4de65d0e53e9220eeff0c825e08fb0139f
itpex-ops <it@parcelex.in>
2026-07-03 10:37:14 +0530

    accounts_customers

--- accounts/views.py
+++ accounts/views.py
@@ -170,7 +170,7 @@ def customer_accounts(request):
     enquiries = Enquiry.objects.all().order_by("-id")
     data = []
     for enquiry in enquiries:
-        vehicle = CustomerTransaction.objects.filter(enquiry=enquiry).first()
+        vehicle = Vehicle.objects.filter(enquiry=enquiry).first()
         total_received = CustomerTransaction.objects.filter(
             enquiry=enquiry
         ).aggregate(
