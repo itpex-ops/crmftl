bae3404cd63853155b99d74a17297f77eaa8bf46
itpex-ops <it@parcelex.in>
2026-09-02 15:31:49 +0530

    update - enq

--- accounts/views.py
+++ accounts/views.py
@@ -958,16 +958,16 @@ def vehicle_ledger(request, vehicle_id):
 
         t.label = label
 
-        return render(
-            request,
-            "accounts/vehicle_ledger.html",
-            {
-                "vehicle": vehicle,
-                "ledger": ledger,
-                'current_balance': current_balance,
-                "transactions": transactions,
-            }
-        )
+    return render(
+        request,
+        "accounts/vehicle_ledger.html",
+        {
+            "vehicle": vehicle,
+            "ledger": ledger,
+            'current_balance': current_balance,
+            "transactions": transactions,
+        }
+    )
 
 def dashboard(request):
 
